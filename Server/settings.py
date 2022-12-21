7384fef8a3c87b509ea135e7e2b3e6b9b001cc77
Alireza Saei <saeialireza359@gmail.com>
2022-12-21 15:44:58 +0330

    basic login/logout page

--- Server/settings.py
+++ Server/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 ASGI_APPLICATION = 'Server.asgi.application'
 
