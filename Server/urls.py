155a228a095efb7ba9bf5c454038c5eebe3b9268
Alireza Saei <saeialireza359@gmail.com>
2023-01-16 13:32:13 +0330

    Setup Media Urls + Default Profile Pic

--- Server/urls.py
+++ Server/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from . import views
- 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
@@ -14,3 +16,5 @@ urlpatterns = [
     path('create_user/', views.create_user),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/models.py
+++ user/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 class custom_user (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField()
+    image = models.ImageField(default='abstract-user.png', null= True, blank=True)
     gender = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=100, unique=True)
